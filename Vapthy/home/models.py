63314149f612db8ebe5e6f963de159b7073a3ad9
rob-nrkis <robh.dev@gmail.com>
2020-08-27 09:48:44 +0200

    First commit

---
+++ Vapthy/home/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    content_presentation = models.CharField(max_length=100, default='SOME STRING')
+    creation_date = models.DateField()
+    creator = models.CharField(max_length=42)
+    description = models.TextField(null=True)
+    photo = models.ImageField(upload_to="galery", default='')
+
+    class Meta:
+        verbose_name = "Compagnies"
+
+    def __str__(self):
+
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    content_presentation = models.CharField(max_length=100, default='SOME STRING')
+    description = models.TextField(null=True)
+    photo = models.ImageField(upload_to="galery", default='')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE) 
+
+    class Meta:
+        verbose_name = "Products"
+
+    def __str__(self):
+
+        return self.name
+
+
+
+
+
+# class Article(models.Model):
+#     titre = models.CharField(max_length=30)
+#     fabricant = models.ForeignKey('Fabricant', on_delete=models.CASCADE)
+#     class Meta:
+#             verbose_name = "article"
+#     def __str__(self):
+
+#         return self.titre
+# <!-- Department.objects.get(password = password, department_name = department_name) -->
+# nt = Article(titre = "crafty+", fabricant = Fabricant_id.objects.get(nom=Storkz))
---
+++ Vapthy/home/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from home.models import Company, Product
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def afficherCompany(request):
+    companies = Company.objects.all()
+    return render(request, 'companies.html', {'all_companies': companies})
+    
+def showAllProducts(request):
+    products = Product.objects.all()
+    return render(request, 'allProducts.html', {'all_products': products})
+
+
+# def afficherCompany(request):
+#     return render(request, 'companies.html')
+
+def companyPage(request, id):
+    company = get_object_or_404(Company, id=id)
+    # product = get_object_or_404(Product, company_id = id)
+    product = Product.objects.filter(company_id = id)
+    # product = Product.objects.filter(company_id = id)
+    return render(request, 'companyPage.html', {'company': company, 'product': product})
+
+# def companyProducts(request, id):
+#     product = get_object_or_404(Product, id=id)
+#     return render(request, 'companyPage.html', {"all_Articles_Company": product})
+
+def detailsProduct(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'detailsProduct.html', {'product': product})
+
